c037d0d8dbe6d4fcc287b1befa49403c3d2d0fee
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-03-21 00:26:58 -0300

    assets como carpeta publica

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express=require('express');
 const app=express();
 const port = 3000;
 
+app.use(express.static('./assets'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.listen(port, () => {
     console.log(`Servidor conectado http://localhost:${port}`)
-})
+});
