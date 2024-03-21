864b4e20d62c803f72996c0413e47627286fcbba
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-03-21 00:42:30 -0300

    trabajo con usuarios

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/abracadabra/usuarios', (req, res) => {
+    res.json(usuarios);
+})
+
 app.listen(port, () => {
     console.log(`Servidor conectado http://localhost:${port}`)
 });
