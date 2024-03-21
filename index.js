824e724f2b16fdc346c6ebfb922013ee83d9bdc8
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-03-21 10:59:02 -0300

    reorganizacion de codigo por errores de donde se habian colocado

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
-const express=require('express');
-const app=express();
+const express = require("express");
+const app = express();
 const port = 3000;
-const usuarios=['Juan', 'Jocelyn','Astrid', 'Maria', 'Ignacia', 'Javier','Brian']
+const usuarios = [
+  "Juan",
+  "Jocelyn",
+  "Astrid",
+  "Maria",
+  "Ignacia",
+  "Javier",
+  "Brian",
+];
 
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.send("Abracadabra");
 });
 
 // ruta generica no encontrada
-app.get('*', (req, res) => {
-    res.send('Esta pagina no existe...');
-})
+app.get("*", (req, res) => {
+  res.send("Esta pagina no existe...");
+});
+
 // ruta usuarios
-app.get('/abracadabra/usuarios', (req, res) => {
+  app.get("/abracadabra/usuarios", (req, res) => {
     res.json(usuarios);
-})
+  });
 
 // buscar conejo
-app.get('/abracadabra/conejo/:id', (req, res) => {
-    const num=req.params.id
-    const numeroMath = Math.floor(Math.random() * 3) + 1;
-    if (num == numeroMath) {
-        res.sendFile(__dirname + './assets/img/conejo.jpg');
-    } else {
-        res.sendFile(__dirname + './assets/img/voldemort.jpg');
-    }
-    res.send('conejo');
-})
+app.get("/abracadabra/conejo/:id", (req, res) => {
+  const num = +req.params.id;
+  const numeroMath = Math.floor(Math.random() * 3) + 1;
 
-// crear midelware
-app.get('/abracadabra/juego/:usuario', (req, res) => {
-    res.sendFile(  __dirname + 'index.html');
+  if (num == numeroMath) {
+    res.sendFile(__dirname + "./assets/img/conejo.jpg");
+  } else {
+    res.sendFile(__dirname + "./assets/img/voldemort.jpg");
+  }
+  res.send("conejo");
+
+  app.use("/abracadabra/juego/:usuario", (req, res, next) => {
+    const usuario = req.params.usuario;
+    const esUsuario = usuarios.map((u) => u.includes(usuarios));
+
+    esUsuario ? next() : res.sendFile(__dirname + "./assets/img/who.jpeg");
+  });
+  // crear midelware
+  app.get("/abracadabra/juego/:usuario", (req, res) => {
+    res.sendFile(__dirname + "index.html");
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Servidor conectado http://localhost:${port}`)
+  console.log(`Servidor conectado http://localhost:${port}`);
 });
